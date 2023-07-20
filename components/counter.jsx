ea1c81a8ad3b45db77e58254f720d6abfa6777f1
Valarie <valarietan@proton.me>
2023-07-20 14:11:22 +0100

    Another commit

---
+++ components/counter.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Counter() {
+  return (
+    <div>
+        <button>-</button>
+        <span>0</span>
+        <button>+</button>
+    </div>
+  )
+}
