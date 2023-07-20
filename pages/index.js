97a8a281a520753bfb4647aef088f8d178f5275d
Valarie <valarietan@proton.me>
2023-07-20 13:57:50 +0100

    Commit counter

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,19 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
+import Counter from '@components/Counter'
+import React from 'react'
+
+
 
 const inter = Inter({ subsets: ['latin'] })
 
-import React from 'react'
+
 
 export default function Home() {
   return (
     <>
-  
+    <Counter />
+
     </>
 
   
