6b92a29479a3ef60b48aa1fbf50721fd053744b6
Valarie <valarietan@proton.me>
2023-07-20 16:06:58 +0100

    editCard to CardList

--- components/CardList.jsx
+++ components/CardList.jsx
@@ -1,14 +1,42 @@
-import React from 'react'
+import React, {useState} from 'react'
 
-export default function CardList({ cards, deleteCard }) { 
-    return (
+export default function CardList({ cards, deleteCard, editCard }) { 
+    const [editIndex, setEditIndex] = useState(null);
+    const [editedCard, setEditedCard] = useState('');
+
+    const handleEdit = (index, card) => {
+        setEditIndex(index);
+        setEditedCard(card);
+      };
+    
+    const handleSave = (index) => {
+        editCard(index, editedCard);
+        setEditIndex(null);
+      };
+
+      return (
         <ul>
           {cards.map((card, index) => (
             <li key={index}>
-              {card}
+              {editIndex === index ? (
+                <>
+                   <input
+                type="text"
+                value={editedCard}
+                onChange={(e) => setEditedCard(e.target.value)}
+              />
+              <button onClick={() => handleSave(index)}>Save</button>
+            </>
+          ) : (
+            <>
+            {todo}
+              <button onClick={() => handleEdit(index, card)}>Edit</button>
               <button onClick={() => deleteCard(index)}>Delete</button>
-            </li>
-          ))}
-        </ul>
-      );
+            </>
+          )}
+        </li>
+      ))}
+    </ul>
+  );
 }
+    
