0105a3328445872fb7147a0a90c91fa62ac756f7
Valarie <valarietan@proton.me>
2023-07-20 18:58:02 +0100

    About added

--- pages/about.js
+++ pages/about.js
@@ -1,1 +1,11 @@
-sfc
+const About = () => {
+    return ( 
+        <div>
+            <h1>Cards Against Negativity</h1>
+            <p>An app that helps to fight self-doubt and build positive vibes.</p>
+            <p>Create cards of positivity to remember how wonderful you are.</p>
+        </div>
+     );
+}
+ 
+export default About ;
