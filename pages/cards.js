bf88a754fcebd1d2cb234fdda5a57c787ea4ee74
Valarie <valarietan@proton.me>
2023-07-21 16:49:26 +0100

    add cards.js

---
+++ pages/cards.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Head from 'next/head'
+
+export default function Cards() {
+  return (
+    <>
+        <Head>
+            <title>Cards Against Negativity</title>
+            <meta name="description" content="All Cards"/>
+        </Head>
+    <div>C</div>
+    </>
+)
+}
