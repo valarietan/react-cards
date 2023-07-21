4b39aaf6a9a8ac8ca5b14ce16cc2b36009fd6a46
Valarie <valarietan@proton.me>
2023-07-21 15:45:38 +0100

    use-debounce added

---
+++ components/hooks/use-debounce.js
@@ -0,0 +1,17 @@
+import {useState, useEffect} from 'react'
+
+export default function use-debounce(value, delay=200) {
+
+    const[debouncedValue, setDebouncedValue] = useState(value)
+
+    useEffect (() => {
+        //
+        const timeOut = setTimeout (() => {
+            setDebouncedValue(value)
+        }, delay)
+
+        return () => clearTimeout(timeOut)
+
+    }, [value])
+
+}
