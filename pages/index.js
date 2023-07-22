e1e9554c7c60976dea6816f5ce930a361a37bbbb
Valarie <valarietan@proton.me>
2023-07-22 18:08:10 +0100

    styled homepage

--- pages/index.js
+++ pages/index.js
@@ -5,28 +5,70 @@ import CreateCardForm from '@/components/CreateCardForm'
 import ReadCardsButton from '@/components/ReadCardsButton'
 import Post from '@/components/Post'
 import Link from 'next/link'
-
+import styled from 'styled-components'
 
 const inter = Inter({ subsets: ['latin'] })
 
+const HomePageContainer = styled.div`
+  text-align: center;
+  padding: 40px;
+  background: #F9E4B7;
+  border-radius: 8px;
+`;
+
+const Title = styled.h1`
+  font-size: 36px;
+  font-weight: bold;
+  margin-bottom: 20px;
+  color: #442C2E;
+`;
+
+const Greeting = styled.p`
+  font-size: 24px;
+  margin-bottom: 30px;
+  color: #442C2E;
+`;
+
+const ButtonsContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  gap: 20px;
+`;
+
+const AddButton = styled.button`
+  padding: 10px 20px;
+  background: #FBBF24;
+  border: none;
+  border-radius: 5px;
+  font-size: 16px;
+  color: white;
+  cursor: pointer;
+`;
+
+const ReadButton = styled.button`
+  padding: 10px 20px;
+  background: #F59E0B;
+  border: none;
+  border-radius: 5px;
+  font-size: 16px;
+  color: white;
+  cursor: pointer;
+`;
+
 const HomePage = () => {
   return (
-    <div>
-      <h1>Cards Against Negativity</h1>
-      <p>Hi, so good to see you.</p>
-      <div>
+    <HomePageContainer>
+      <Title>Cards Against Negativity</Title>
+      <Greeting>Hi, so good to see you.</Greeting>
+      <ButtonsContainer>
         <Link href="/createCard">
-          <a>
-            <button>Add Card</button>
-          </a>
+          <AddButton>Add Card</AddButton>
         </Link>
         <Link href="/cards">
-          <a>
-            <button>Read Card</button>
-          </a>
+          <ReadButton>Read Card</ReadButton>
         </Link>
-      </div>
-    </div>
+      </ButtonsContainer>
+    </HomePageContainer>
   );
 };
 
