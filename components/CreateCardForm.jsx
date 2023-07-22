d868c7ac10be15a421c65e5b047881c2c5f6d82e
Valarie <valarietan@proton.me>
2023-07-22 18:10:55 +0100

    styled create card form

--- components/CreateCardForm.jsx
+++ components/CreateCardForm.jsx
@@ -1,12 +1,42 @@
-import { useState } from 'react';
-import styled from 'styled-components'
+import React, { useState } from 'react';
+import styled from 'styled-components';
 
 const AppWrapper = styled.form`
   max-width: 400px;
   margin: 0 auto;
   padding: 20px;
   box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
-`
+`;
+
+const Title = styled.h2`
+  font-size: 1.5rem;
+  margin-bottom: 1rem;
+`;
+
+const TextArea = styled.textarea`
+  width: 100%;
+  padding: 8px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  resize: vertical;
+  min-height: 100px;
+`;
+
+const SubmitButton = styled.button`
+  display: block;
+  margin-top: 10px;
+  padding: 8px 16px;
+  background-color: #FBBF24;
+  color: white;
+  border: none;
+  border-radius: 20px;
+  cursor: pointer;
+  transition: background-color 0.2s;
+
+  &:hover {
+    background-color: #F59E0B;
+  }
+`;
 
 const CreateCardForm = ({ onSave }) => {
   const [card, setCard] = useState('');
@@ -23,16 +53,15 @@ const CreateCardForm = ({ onSave }) => {
 
   return (
     <AppWrapper onSubmit={handleSubmit}>
-    <h2 className='text-2xl'>Create a new card</h2>
-      <textarea 
+      <Title>Create a new card</Title>
+      <TextArea
         value={card}
         onChange={handleCardChange}
         placeholder="Type the good stuff..."
         rows="7"
-        cols="42"
         required
       />
-      <button className='rounded-full hover:rounded-lg' type="submit">Save it!</button>
+      <SubmitButton type="submit">Save it!</SubmitButton>
     </AppWrapper>
   );
 };
