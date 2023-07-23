ee47d788b5f50950dcf603e22f98ff7eed143de7
Valarie <valarietan@proton.me>
2023-07-23 13:24:17 +0100

    backup

--- components/ReadCard.jsx
+++ components/ReadCard.jsx
@@ -1,17 +1,2 @@
-import React from 'react'
 
-const AllCards = ({cards}) => {
-    return (
-        <ul>
-            {cards.map((card)=> (
-                <li key={card.id}>
-                <span>{card.text}</span>
-                <button><Read></button>
-                </li>
-            ))}
-        </ul>
-      );
-}; 
- 
-export default AllCards;
 
