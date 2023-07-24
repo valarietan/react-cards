503bde4f2d04499096c7a89843e49223bedb06ea
Valarie <valarietan@proton.me>
2023-07-24 12:14:58 +0100

    placeholder opening line

--- components/CreateCardForm.jsx
+++ components/CreateCardForm.jsx
@@ -88,7 +88,7 @@ return (
       <TextArea
         value={card}
         onChange={handleCardChange}
-        placeholder="Type what makes you feel good..."
+        placeholder="Type something nice..."
         maxLength={300}
         rows="7"
         required
