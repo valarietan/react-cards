c4458a9e92d8d6e8979fa0a6cb0944941c868c26
Valarie <valarietan@proton.me>
2023-07-25 14:43:55 +0100

    Commit this

--- components/CardSavedPage.jsx
+++ components/CardSavedPage.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
+import Link from 'next/link'
 
 const Wrapper = styled.div`
   max-width: 400px;
@@ -19,11 +20,64 @@ const Message = styled.p`
   color: #555;
 `;
 
+const ReturnButton = styled.button`
+  margin-top: 10px;
+  padding: 8px 16px;
+  background-color: #FBBF24;
+  color: white;
+  border: none;
+  border-radius: 20px;
+  cursor: pointer;
+  transition: background-color 0.2s;
+
+  &:hover {
+    background-color: #F59E0B;
+  }
+`;
+
+const WriteAnotherButton = styled.button`
+  margin-top: 10px;
+  padding: 8px 16px;
+  background-color: #4CAF50;
+  color: white;
+  border: none;
+  border-radius: 20px;
+  cursor: pointer;
+  transition: background-color 0.2s;
+
+  &:hover {
+    background-color: #45a049;
+  }
+`;
+const ReadCardButton = styled.button`
+  margin-top: 10px;
+  padding: 8px 16px;
+  background-color: #3498db;
+  color: white;
+  border: none;
+  border-radius: 20px;
+  cursor: pointer;
+  transition: background-color 0.2s;
+
+  &:hover {
+    background-color: #2980b9;
+  }
+`;
+
 const CardSavedPage = () => {
   return (
     <Wrapper>
       <Title>Card Saved!</Title>
-      <Message>Your card has been saved successfully.</Message>
+      <Message>Great job. What would you like to do next?</Message>
+      <Link href="/">
+          <ReturnButton>Return to the homepage?</ReturnButton>
+      </Link>
+      <Link href="/create">
+          <WriteAnotherButton>Write another card?</WriteAnotherButton>
+        </Link>
+        <Link href="/cards">
+            <ReadCardButton>Read a card?</ReadCardButton>
+        </Link>
     </Wrapper>
   );
 };
