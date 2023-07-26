88b5cb7ad99e0dd4dc2948b8fa2822afe51dac71
Valarie <valarietan@proton.me>
2023-07-26 14:39:08 +0100

    work please

--- pages/cards.js
+++ pages/cards.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import Head from 'next/head';
 import Link from 'next/link';
 import styled from 'styled-components';
