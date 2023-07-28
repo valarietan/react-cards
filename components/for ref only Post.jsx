bd36d877b2c5e6855341c029e6545ccffea190ec
Valarie <valarietan@proton.me>
2023-07-28 09:37:47 +0100

    commit

--- components/for ref only Post.jsx
+++ components/for ref only Post.jsx
@@ -1,48 +1,48 @@
-// import React, {useEffect, useState} from 'react'
+import React, {useEffect, useState} from 'react'
 
-// function PostCard({post}){
-//     return(
-//         <div>
-//             <h1>{post.title}</h1>
-//             <p>{post.body}</p>
-//         </div>
-//     )
-// }
+function PostCard({post}){
+    return(
+        <div>
+            <h1>{post.title}</h1>
+            <p>{post.body}</p>
+        </div>
+    )
+}
 
-// export default function Post() {
-//     const [postData, setPostData] = useState([])
-//     const [searchTerm, setSearchTerm] = useState('')
+export default function Post() {
+    const [postData, setPostData] = useState([])
+    const [searchTerm, setSearchTerm] = useState('')
 
-//     const[originalCopy, setOriginalCopy] = useState([])
+    const[originalCopy, setOriginalCopy] = useState([])
 
-//     const debouncedValue = useDebounce(searchTerm)
+    const debouncedValue = useDebounce(searchTerm)
 
-//     useEffect(() => {
-//         fetch('https://jsonplaceholder.typicode.com/posts')
-//         .then((response) => response.json())
-//         .then((json) => {   
-//             setPostData(json)
-//             setOriginalCopy(json)
-//         });
+    useEffect(() => {
+        fetch('https://jsonplaceholder.typicode.com/posts')
+        .then((response) => response.json())
+        .then((json) => {   
+            setPostData(json)
+            setOriginalCopy(json)
+        });
          
-//         }, []) 
+        }, []) 
 
-//     useEffect(() => {
-//         if(!debouncedValue){
-//             setPostData(originalCopy)
-//             return
-//         }
-//         const filteredPosts = postData.filter(post => post.title.includes
-//             (debouncedValue.toLowerCase()))
-// } ,     [debouncedValue])
+    useEffect(() => {
+        if(!debouncedValue){
+            setPostData(originalCopy)
+            return
+        }
+        const filteredPosts = postData.filter(post => post.title.includes
+            (debouncedValue.toLowerCase()))
+} ,     [debouncedValue])
         
-//     return(
-//         <div className='flex flex-col space-y-2'>
-//             <input onInput={(e) => setSearchTerm(e.target.value)} value={searchTerm}
-//             type ="search" className="border p-2 rounded" />
+    return(
+        <div className='flex flex-col space-y-2'>
+            <input onInput={(e) => setSearchTerm(e.target.value)} value={searchTerm}
+            type ="search" className="border p-2 rounded" />
             
-//             {postData.map(singlePost => <PostCard key={singlePost.id} post={singlePost} />)}
-//         </div>
-//     )
-// }
+            {postData.map(singlePost => <PostCard key={singlePost.id} post={singlePost} />)}
+        </div>
+    )
+}
    
